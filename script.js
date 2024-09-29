ebe9d5006ace0245af2a93505de7a00037af7dab
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-29 16:42:39 +0500

    Adding check to handle the negative month and days

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function calculateAge() {
     let years = today.getFullYear() - birthDate.getFullYear();
     let months = today.getMonth() - birthDate.getMonth();
     let days = today.getDate() - birthDate.getDate(); 
+
+    if (days < 0) {
+        months--;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+    
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
     document.getElementById("year").textContent = years;
     document.getElementById("month").textContent = months;
     document.getElementById("day").textContent = days;
