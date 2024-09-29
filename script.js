1424e4e48e2a03728ccc55ede1de0bbeaa57d631
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-29 15:54:33 +0500

    declear variables for user input, years , months and days

---
+++ script.js
@@ -0,0 +1,4 @@
+var birthInput = document.getElementById("birth-input");
+var years = document.getElementById("year");
+var months = document.getElementById("month");
+var days = document.getElementById("day");
