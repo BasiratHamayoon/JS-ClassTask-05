7dfcc6f73900ec7c6d443aeb91be976b63e5b066
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-02 14:35:10 +0500

    Adding check for future year

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function calculateAge() {
     
     if (birthInput.trim() === "") {
         alert("Birth input cannot be empty!");
+        return;
     }
-    
     var birthDate = new Date(birthInput);
     var today = new Date();
     let years = today.getFullYear() - birthDate.getFullYear();
     let months = today.getMonth() - birthDate.getMonth();
     let days = today.getDate() - birthDate.getDate(); 
-
+    if (birthDate > today){
+        alert("Birth year cannot be in the future!");
+        return;
+    }
     if (days < 0) {
         months--;
         days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
